7050fdb8c04b1264134806e1160a745656092084
Minu <mezu.mezu@hotmail.com>
2024-11-01 11:27:56 +0200

    svgicons updated

--- src/app/svg/SvgIcons.tsx
+++ src/app/svg/SvgIcons.tsx
@@ -776,17 +776,18 @@ export function ChartGraphSvg(props: React.SVGProps<SVGSVGElement>) {
 export function NextJsSvg(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg
+      {...props}
       fill="#000000"
       width="20px"
       height="20px"
       viewBox="0 0 32 32"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         {" "}
